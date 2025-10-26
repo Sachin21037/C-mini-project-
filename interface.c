efea6385ba747e7acce6f0eb59f8f0daff4f0b7a
Sachin21037 <Sachinshehan1234@gmail.com>
2025-10-26 12:30:45 +0530

    create interface.c
    project interface

---
+++ interface.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <string.h>
+
+void adminlog();
+void registerpartie();
+void candidate_reg();
+void votecast();
+void voterreg();
+void votecount();
+
+void main()
+{
+    adminlog();
+    return;
+}
+
+void adminlog()
+{
+    FILE *admlog;
+    char password[20];
+    char adminpass[20];
+    char adpass[20];
+    char line[100];
+    int choice;
+    printf("\t\t\t\t---------------------------------------------------------------------------------------------------\n");
+    printf("\t\t\\t\t\t\t\t\ttWelcome to Sri Lankan Parliament Election Voting System\n");
+    printf("\t\t\t\t---------------------------------------------------------------------------------------------------\n\n");
+    printf("\t\t\t\t\t\t\t\t** ADMIN login **\n\n");
+
+    admlog = fopen("admin.txt", "r");
+
+    printf("enter admin password:");
+    scanf("%s", adpass);
+    FILE *adminfile;
+    adminfile = fopen("admin.txt", "r");
+    if (adminfile == NULL)
+    {
+        printf("Error: Could not open admin.txt!! \ncheck admin details are saved.\n");
+        return;
+    }
+    fgets(line, sizeof(line), admlog); // 1st line
+    fgets(line, sizeof(line), admlog); // 2nd line
+    fscanf(admlog, "%s", adminpass);
+    if ((strcmp(adminpass, adpass)) == 0)
+
+    {
+        printf("\t\t\t\t---------------------------------------------------------------------------------------------------\n");
+        printf("\t\t\\t\t\t\t\t\ttWelcome to Sri Lankan Parliament Election Voting System\n");
+        printf("\t\t\t\t---------------------------------------------------------------------------------------------------\n\n");
+        printf("\t\t\t\t\t\t\t\t** ADMIN PANEL  **\n\n");
+
+        printf("\t\t\t\t\t\t\tAccess Granted!\n");
+        do
+        {
+            printf("\t\t\t\t\t\t\tChose your number:\n\n");
+
+            printf("\t\t\t\t\t\t\t1: open party registration\n");
+            printf("\t\t\t\t\t\t\t2: open candidate registration\n");
+            printf("\t\t\t\t\t\t\t3: start voter registration\n");
+
+            printf("\t\t\t\t\t\t\t4: start vote cast\n");
+            printf("\t\t\t\t\t\t\t5: start vote counting\n");
+            printf("\t\t\t\t\t\t\t6: exit admin panel\n");
+
+            printf("\n\t\tPlease Enter Your Number:");
+
+            scanf(" %d", &choice);
+
+            switch (choice)
+            {
+            case 1:
+                registerpartie();
+                break;
+            case 2:
+                candidate_reg();
+                break;
+            case 3:
+                voterreg();
+                break;
+            case 4:
+                votecast();
+                break;
+
+            case 5:
+                votecount();
+
+            case 6:
+                printf("Exiting Admin Panel. Goodbye!\n");
+                break;
+            default:
+                printf("Invalid choice! enter again!!!\n");
+            }
+            
+
+        } while (choice != 6);
+    }
+    else
+    {
+        printf("Access Denied! Incorrect Password.\n");
+    }
+}
